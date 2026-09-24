86ae8fc79e6ac6b2c0b86f15f116219d8a1bc8be
HenriqueXaviercj <henrique.xavier.cj@gmail.com>
2026-09-24 07:57:10 -0300

    feat: add not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NotFount() {
+  return (
+    <section className="container">
+      <h1 className="title">Página não encontrada</h1>
+      <Link
+        className="button"
+        href={"/"}
+        style={{ marginBottom: "1rem", display: "inline-block" }}
+      >
+        Voltar para a home.
+      </Link>
+    </section>
+  );
+}
